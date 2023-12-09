b2f7eb21b194e043ca17b49f30fa2be7f8de190b
NnayaSh <149097893+NnayaSh@users.noreply.github.com>
2023-12-09 12:55:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,8 @@ document.getElementById("Forma").addEventListener("submit", function(event) {
         data[pair[0]] = pair[1];
     }
     let resultsDiv = document.getElementById("results");
-    resultsDiv.innerHTML = "";
-
     let jsonOutput = JSON.stringify(data, null, 4);
-    let jsonElement = document.createElement("pre");
-    jsonElement.textContent = jsonOutput;
-    resultsDiv.appendChild(jsonElement);
+    resultsDiv.innerHTML = textContent = jsonOutput;
 
     console.log(jsonOutput);
 });
